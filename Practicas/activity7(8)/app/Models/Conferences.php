39421b9a4adc46d4abd902e60ec6ed9f850ddd5d
KevinFlores19 <145875283+KevinFlores19@users.noreply.github.com>
2024-03-16 21:48:59 -0600

    Modificaciones para la actividad 8

--- Practicas/activity7(8)/app/Models/Conferences.php
+++ Practicas/activity7(8)/app/Models/Conferences.php
@@ -5,9 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Cities extends Model
+class Conferences extends Model
 {
     use HasFactory;
-    protected $table = "cities";
-    
+
+    protected $table = "conferences";
+    public $timestamps = false;
 }
--- Practicas/activity7(8)/app/Models/Divisions.php
+++ Practicas/activity7(8)/app/Models/Divisions.php
@@ -5,9 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Cities extends Model
+class Divisions extends Model
 {
     use HasFactory;
-    protected $table = "cities";
-    
+
+    protected $table = "divisions";
+    public $timestamps = false;
 }
