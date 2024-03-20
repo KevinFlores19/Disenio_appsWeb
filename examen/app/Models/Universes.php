bd9c037d41841491b1ded1339260e40833264645
KevinFlores19 <145875283+KevinFlores19@users.noreply.github.com>
2024-03-20 17:38:15 -0600

    examen

---
+++ examen/app/Models/Universes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Universes extends Model
+{
+    use HasFactory;
+    protected $table = "universes";
+	public $timestamps = false;
+}
